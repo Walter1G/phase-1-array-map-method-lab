9a55e489c7bac4191cdadaa722fc02470760c2f4
Walter N <2760walter@gmail.com>
2023-09-29 19:31:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,45 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = tutorials.map(tutorial =>) 
+
+let sentence ='what is JSONP?';
+
+function titleCased(){
+
+  return tutorials.map( function (tutorial){
+    return tutorial.split(" ").map(function (word){
+      return word.charAt(0).toUpperCase()+ word.slice(1);
+    }).join(" ")  
+  })
+   
+  }
+
+
+console.log(toTitleCase());
+
+
+
+
+// breakSentence(sentence);
+// console.log((sentence.split("")).charAt(0).toUpperCase()+ word.substr(1).toLowerCase());
+
+
+
+// function capitalizeWord(word){
+//   return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+// }
+
+// function breakSentenceAndCapitalize(str){
+//   return str.split(" ").map(function (word){
+//     return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+//   });
+
+// }
+
+// console.log(breakSentenceAndCapitalize("ths is a sentence"))
+
+// console.log(capitalizeWord("sherry"))
+
+// breakSentence(sentence);
+
